152d4f6967970fe134ceb7002880633099725c88
Danilx8 <lugovskihdanil@yandex.ru>
2023-05-25 21:37:31 +0700

    somewhat ready class
    обе операции (insert и remove) имеют в худшем случае сложность O(n)

--- da/dynamic array test/UnitTest1.cs
+++ da/dynamic array test/UnitTest1.cs
@@ -1,11 +1,81 @@
-namespace dynamic_array_test
+using System.Runtime.CompilerServices;
+
+namespace AlgorithmsDataStructures
 {
     [TestClass]
     public class UnitTest1
     {
-        [TestMethod]
-        public void TestMethod1()
+        DynArray<int> bigArray;
+        DynArray<int> emptyArray;
+        const int LENGTH = 16;
+
+        [TestInitialize]
+        public void TestInitialize()
         {
+            bigArray    = new DynArray<int>();
+            emptyArray  = new DynArray<int>();
+            
+            for (int i = 0; i < LENGTH; ++i)
+            {
+                bigArray.Append(i);
+            }
+        }
+
+        [TestMethod]
+        public void EmptyInsert()
+        {
+            emptyArray.Insert(8, 0);
+            CollectionAssert.AreEqual(new int[] { 16, 1, 8 }, 
+                new int[] { emptyArray.capacity, emptyArray.count, emptyArray.GetItem(0) });
+        }
+
+        [TestMethod]
+        public void BigInsert()
+        {
+            int newValue = 12;
+            bigArray.Insert(newValue, 0);
+            CollectionAssert.AreEqual(new int[] { 32, 17, newValue }, 
+                new int[] { bigArray.capacity, bigArray.count, bigArray.GetItem(0) });
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(IndexOutOfRangeException),"Index out of range.")]
+        public void WrongInsert()
+        {
+            int newValue = 12;
+            //bigArray.Insert(newValue, -2);
+            bigArray.Insert(newValue, 30);
+            //EmpyArray.Insert(newValue, 1);
+        }
+
+        [TestMethod]
+        public void NonShrinkingRemove()
+        {
+            int deletedValue = 12;
+            bigArray.Append(deletedValue);
+            int followingValue = 18;
+            bigArray.Append(followingValue);
+            bigArray.Remove(16);
+            CollectionAssert.AreEqual(new int[] { 17, 32, followingValue }, 
+                new int[] { bigArray.count, bigArray.capacity, bigArray.GetItem(16)});
+        }
+
+        [TestMethod]
+        public void ShrinkingRemove()
+        {
+            int deletedValue = 12;
+            bigArray.Append(deletedValue);
+            bigArray.Remove(16);
+            bigArray.Remove(0);
+            CollectionAssert.AreEqual(new int[] { 15, (int)(32 / 3 * 2), LENGTH - 1}, 
+                new int[] { bigArray.count, bigArray.capacity, bigArray.GetItem(14) });
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(IndexOutOfRangeException), "Index out of range.")]
+        public void FailedRemove()
+        {
+            emptyArray.Remove(9);
         }
     }
 }
---
+++ da/dynamic array/dynamic array.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AlgorithmsDataStructures
+{
+    public class DynArray<T>
+    {
+        const float SHRINK_FRACTION = 0.5F;
+        public T[] array;
+        public int count;
+        public int capacity;
+
+        public DynArray()
+        {
+            count = 0;
+            MakeArray(16);
+        }
+
+        public void MakeArray(int new_capacity)
+        {
+            T[] newArray = new T[new_capacity];
+            int transferringCapacity = Math.Min(capacity, new_capacity);
+            if (!(array is null) && array != Array.Empty<T>()) Array.Copy(array, newArray, transferringCapacity);
+            capacity = new_capacity;
+            array = newArray;
+        }
+
+        public T GetItem(int index)
+        {
+            if (index < 0 || index >= count) throw new IndexOutOfRangeException("Index out of range.");
+            return array[index];
+        }
+
+        public void Append(T itm)
+        {
+            if (count + 1 > capacity) MakeArray(capacity * 2);
+            array[count++] = itm;
+        }
+
+        public void Insert(T itm, int index)
+        {
+            if (index < 0 || index > count) throw new IndexOutOfRangeException("Index out of range.");
+            if (++count > capacity) MakeArray(capacity * 2);
+
+            for (int i = index + 1; i < count; ++i)
+            {
+                array[i] = array[i - 1];
+            }
+            array[index] = itm;
+        }
+
+        public void Remove(int index)
+        {
+            if (index < 0 || index >= count) throw new IndexOutOfRangeException("Index out of range.");
+            for (int i = index; i < count; ++i)
+            {
+                array[i] = array[i + 1];
+            }
+
+            if (--count < SHRINK_FRACTION * capacity) MakeArray(Math.Max((int)(capacity / 3 * 2), 16));
+        }
+    }
+}
