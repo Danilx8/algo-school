7042d1617c80180b5c05748a828795c5e15f74c5
Danilx8 <lugovskihdanil@yandex.ru>
2023-05-28 21:58:27 +0700

    ready

--- dq/Deque test/UnitTest1.cs
+++ dq/Deque test/UnitTest1.cs
@@ -1,11 +1,38 @@
-namespace Deque_test
+namespace AlgorithmsDataStructures
 {
     [TestClass]
-    public class UnitTest1
+    public class DequeTest
     {
-        [TestMethod]
-        public void TestMethod1()
+        Deque<int> emptyDeque;
+        Deque<int> singleDeque;
+        Deque<int> bigDeque;
+
+        [TestInitialize]
+        public void TestInitialize()
         {
+            emptyDeque = new Deque<int>();
+            singleDeque = new Deque<int>();
+            bigDeque = new Deque<int>();
+
+            singleDeque.AddFront(1);
+
+            for (int i = 0; i < 10; ++i)
+            {
+                bigDeque.AddFront(i);
+            }
+        }
+
+        [TestMethod]
+        public void emptyDequeSize()
+        {
+            Assert.AreEqual(0, emptyDeque.Size());
+        }
+
+        [TestMethod]
+        public void singleAddDifference()
+        {
+            CollectionAssert.AreEqual(new int[] { 1, 1 }, 
+                new int[] { singleDeque.Size(), singleDeque.RemoveTail() });
         }
     }
 }
--- dq/Deque/Deque class.cs
+++ dq/Deque/Deque class.cs
@@ -3,40 +3,88 @@ using System.Collections.Generic;
 
 namespace AlgorithmsDataStructures
 {
-
-    class Deque<T>
+    public class Node<T>
     {
+        public T value;
+        public Node<T> next;
+        public Node<T> prev;
+
+        public Node() { }
+
+        public Node(T _value) { value = _value; }
+    }
+
+    public class DummyNode<T> : Node<T>
+    {
+        public DummyNode()
+        {
+            next = null;
+            prev = null;
+        }
+    }
+
+    public class Deque<T>
+    {
+        DummyNode<T> edge;
+
         public Deque()
         {
-            // инициализация внутреннего хранилища
+            edge = new DummyNode<T>();
+            edge.next = edge;
+            edge.prev = edge;
         }
 
         public void AddFront(T item)
         {
-            // добавление в голову
+            Node<T> node = new Node<T>(item);
+            
+            node.next = edge.next;
+            edge.next.prev = node;
+            edge.next = node;
+            node.prev = edge;
         }
 
         public void AddTail(T item)
         {
-            // добавление в хвост
+            Node<T> node = new Node<T>(item);
+
+            node.prev = edge.prev;
+            edge.prev.next = node;
+            edge.prev = node;
+            node.next = edge;
         }
 
         public T RemoveFront()
         {
-            // удаление из головы
-            return default(T);
+            if (edge.next is DummyNode<T> ) return default(T);
+
+            T value = edge.next.value;
+            edge.next.next.prev = edge.next;
+            edge.next = edge.next.next;
+            return value;
         }
 
         public T RemoveTail()
         {
-            // удаление из хвоста
-            return default(T);
+            if (edge.prev is DummyNode<T>) return default(T);
+
+            T value = edge.prev.value;
+            edge.prev.prev.next = edge.prev;
+            edge.prev = edge.prev.prev;
+            return value;
         }
 
         public int Size()
         {
-            return 0; // размер очереди
+            Node<T> node = edge.next;
+            int count = 0;
+
+            while (!(node is DummyNode<T>))
+            {
+                ++count;
+                node = node.next;
+            }
+            return count;
         }
     }
-
 }
