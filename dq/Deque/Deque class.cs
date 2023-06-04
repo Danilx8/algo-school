6a4562bc0210f85ded544678e36e7b9c13d3ff3a
Danilx8 <lugovskihdanil@yandex.ru>
2023-06-04 20:37:51 +0700

    syntax fix

--- dq/Deque/Deque class.cs
+++ dq/Deque/Deque class.cs
@@ -44,7 +44,7 @@ namespace AlgorithmsDataStructures
             return default;
         }
 
-        public int Count()
+        public int Size()
         {
             return elements.Count;
         }
