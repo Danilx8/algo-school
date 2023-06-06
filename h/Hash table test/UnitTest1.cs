c0291862e9b737c7edfc9f343e35998ca1ff80cb
Danilx8 <lugovskihdanil@yandex.ru>
2023-06-06 17:48:20 +0700

    init

---
+++ h/Hash table test/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace Hash_table_test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ h/Hash table/Hash class.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AlgorithmsDataStructures
+{
+
+    public class HashTable
+    {
+        public int size;
+        public int step;
+        public string[] slots;
+
+        public HashTable(int sz, int stp)
+        {
+            size = sz;
+            step = stp;
+            slots = new string[size];
+            for (int i = 0; i < size; i++) slots[i] = null;
+        }
+
+        public int HashFun(string value)
+        {
+           return value.GetHashCode() % size;
+        }
+
+        public int SeekSlot(string value)
+        {
+            int index = HashFun(value);
+            int visitedAmount = 0;
+
+            while (visitedAmount < size)
+            {
+                if (slots[index] is null) return index;
+
+                ++visitedAmount;
+                index += step;
+                if (index > size) index -= size;
+            }
+
+            return -1;
+        }
+
+        public int Put(string value)
+        {
+            int index = SeekSlot(value);
+
+            if (index != -1)
+            {
+                slots[index] = value;
+                return index;
+            }
+            return -1;
+        }
+
+        public int Find(string value)
+        {
+            int index = SeekSlot(value);
+
+            if (index == -1) return -1;
+
+            if (slots[index] == value) return index;
+            return -1;
+        }
+    }
+
+}
