4a25401871989d446fc29547b94be264cc95ca68
Danilx8 <lugovskihdanil@yandex.ru>
2023-06-14 01:12:06 +0700

    new hombrew bitArray

--- bf/Bloom filter/Filter class.cs
+++ bf/Bloom filter/Filter class.cs
@@ -7,22 +7,48 @@ namespace AlgorithmsDataStructures
 {
     public class ArrayOfBits
     {
-        public bool[] Bits { get; set; }
+        private int[] data;
+        private const int BitsPerInt = 32;
 
         public ArrayOfBits(int size)
+        { 
+            int arraySize = (size + BitsPerInt - 1) / BitsPerInt;
+            data = new int[arraySize];
+        }
+
+        public bool Get(int index)
         {
-            Bits = new bool[size];
+            int intIndex = index / BitsPerInt;
+            int bitOffset = index % BitsPerInt;
+
+            return ((data[intIndex] >> bitOffset) & 1) != 0;
+        }
+
+        public void Set(int index, bool value)
+        {
+            int intIndex = index / BitsPerInt;
+            int bitOffset = index % BitsPerInt;
+
+            if (value)
+            {
+                data[intIndex] |= (1 << bitOffset);
+            }
+            else
+            {
+                data[intIndex] &= ~(1 << bitOffset);
+            }
         }
     }
 
     public class BloomFilter
     {
+        
         public int filter_len;
         public ArrayOfBits array;
 
         public BloomFilter(int f_len)
         {
-            filter_len = f_len;
+            filter_len = 32;
             array = new ArrayOfBits(filter_len);
         }
 
@@ -55,12 +81,13 @@ namespace AlgorithmsDataStructures
 
         public void Add(string str1)
         {
-            array.Bits[Hash1(str1)] = array.Bits[Hash2(str1)] = true;
+            array.Set(Hash1(str1), true);
+            array.Set(Hash2(str1), true);
         }
 
         public bool IsValue(string str1)
         {
-            if (array.Bits[Hash1(str1)] && array.Bits[Hash2(str1)]) return true;
+            if (array.Get(Hash1(str1)) && array.Get(Hash2(str1))) return true;
             return false;
         }
     }
