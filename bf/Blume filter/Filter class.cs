73ffba3b2bb0f20d7b7894fa4617a498c5961e32
Danilx8 <lugovskihdanil@yandex.ru>
2023-06-13 22:23:24 +0700

    first realisation

--- bf/Blume filter/Filter class.cs
+++ bf/Blume filter/Filter class.cs
@@ -1,45 +1,60 @@
 ﻿using System.Collections.Generic;
 using System;
 using System.IO;
+using System.Linq.Expressions;
 
 namespace AlgorithmsDataStructures
 {
     public class BloomFilter
     {
         public int filter_len;
+        public byte[] array;
 
         public BloomFilter(int f_len)
         {
             filter_len = f_len;
-            // создаём битовый массив длиной f_len ...
+            array = new byte[filter_len];
+            for (int i = 0; i < filter_len; ++i)
+            {
+                array[i] = 0;
+            }
         }
 
-        // хэш-функции
         public int Hash1(string str1)
         {
-            // 17
+            const int multiplier = 17;
+            int sum = 0;
+
             for (int i = 0; i < str1.Length; i++)
             {
                 int code = (int)str1[i];
+                sum *= multiplier;
+                sum += code;
             }
-            // реализация ...
-            return 0;
+            return sum % filter_len;
         }
         public int Hash2(string str1)
         {
-            // 223
-            // реализация ...
-            return 0;
+            const int multiplier = 223;
+            int sum = 0;
+
+            for (int i = 0; i < str1.Length; i++)
+            {
+                int code = (int)str1[i];
+                sum *= multiplier;
+                sum += code;
+            }
+            return sum % filter_len;
         }
 
         public void Add(string str1)
         {
-            // добавляем строку str1 в фильтр
+            array[Hash1(str1)] = array[Hash2(str1)] = 1;
         }
 
         public bool IsValue(string str1)
         {
-            // проверка, имеется ли строка str1 в фильтре
+            if (array[Hash1(str1)] == 1 && array[Hash2(str1)] == 1) return true;
             return false;
         }
     }
