a1a11f7f246967ff6448612f5329fed0f231054d
Danilx8 <lugovskihdanil@yandex.ru>
2023-06-13 22:47:00 +0700

    enhanced the code and changed all ints to UInt64's

--- bf/Blume filter/Filter class.cs
+++ bf/Blume filter/Filter class.cs
@@ -7,41 +7,37 @@ namespace AlgorithmsDataStructures
 {
     public class BloomFilter
     {
-        public int filter_len;
+        public UInt64 filter_len;
         public byte[] array;
 
-        public BloomFilter(int f_len)
+        public BloomFilter(UInt64 f_len)
         {
             filter_len = f_len;
             array = new byte[filter_len];
-            for (int i = 0; i < filter_len; ++i)
-            {
-                array[i] = 0;
-            }
         }
 
-        public int Hash1(string str1)
+        public UInt64 Hash1(string str1)
         {
-            const int multiplier = 17;
-            int sum = 0;
+            const int MULTIPLIER = 17;
+            UInt64 sum = 0;
 
             for (int i = 0; i < str1.Length; i++)
             {
-                int code = (int)str1[i];
-                sum *= multiplier;
+                UInt64 code = (UInt64)str1[i];
+                sum *= MULTIPLIER;
                 sum += code;
             }
             return sum % filter_len;
         }
-        public int Hash2(string str1)
+        public UInt64 Hash2(string str1)
         {
-            const int multiplier = 223;
-            int sum = 0;
+            const int MULTIPLIER = 223;
+            UInt64 sum = 0;
 
             for (int i = 0; i < str1.Length; i++)
             {
-                int code = (int)str1[i];
-                sum *= multiplier;
+                UInt64 code = (UInt64)str1[i];
+                sum *= MULTIPLIER;
                 sum += code;
             }
             return sum % filter_len;
