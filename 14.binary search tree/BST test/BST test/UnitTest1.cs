5c6afb8ddfdacc94a621d0ade47f801abb8680bc
Луговских Данил Викторович <lugovskihdanil@yandex.ru>
2023-08-12 02:27:55 +0700

    passed tests version

--- 14.binary search tree/BST test/BST test/UnitTest1.cs
+++ 14.binary search tree/BST test/BST test/UnitTest1.cs
@@ -1,11 +1,108 @@
+using AlgorithmsDataStructures2;
+
 namespace BST_test
 {
     [TestClass]
     public class UnitTest1
     {
-        [TestMethod]
-        public void TestMethod1()
+        BST<int> tree;
+        BST<int> emptyTree;
+        BSTNode<int> root;
+
+        [TestInitialize]
+        public void TestInitialize()
         {
+            root = new BSTNode<int>(0, 0, null);
+            tree = new BST<int>(root);
+            emptyTree = new BST<int>(null);
+        }
+
+        [TestMethod]
+        public void AddLeft()
+        {
+            Assert.IsFalse(tree.FindNodeByKey(-1).NodeHasKey);
+            tree.AddKeyValue(-1, 1);
+            BSTFind<int> firstSupposedResult = tree.FindNodeByKey(-1);
+            Assert.IsTrue(firstSupposedResult.NodeHasKey);
+
+            Assert.IsFalse(emptyTree.FindNodeByKey(0).NodeHasKey);
+            emptyTree.AddKeyValue(0, 0);
+            BSTFind<int> secondSupposedResult = emptyTree.FindNodeByKey(0);
+            Assert.IsTrue(secondSupposedResult.NodeHasKey);
+        }
+
+        [TestMethod]
+        public void AddRight()
+        {
+            tree.AddKeyValue(1, 1);
+            BSTFind<int> firstSupposedResult = tree.FindNodeByKey(1);
+            Assert.IsTrue(firstSupposedResult.NodeHasKey);
+        }
+
+        [TestMethod]
+        public void AddExistingKey()
+        {
+            Assert.IsFalse(tree.AddKeyValue(0, 0));
+        }
+
+        [TestMethod]
+        public void RootMaxSearch()
+        {
+            for (int i = 0; i < 10; ++i)
+            {
+                tree.AddKeyValue(i, i);
+                Assert.AreEqual(i, tree.FinMinMax(root, true).NodeKey);
+            }
+        }
+
+        [TestMethod]
+        public void RootMinSearch()
+        {
+            for (int i = 1; i < 11; ++i)
+            {
+                tree.AddKeyValue(i, i);
+                Assert.AreEqual(0, tree.FinMinMax(root, false).NodeKey);
+            }
+        }
+
+        [TestMethod]
+        public void NodeMaxSearch()
+        {
+            tree.AddKeyValue(12, 12);
+            for (int i = 0; i < 10; ++i)
+            {
+                tree.AddKeyValue(i, i);  
+            }
+            BSTNode<int> subNode = tree.FindNodeByKey(9).Node;
+            Assert.AreEqual(9, tree.FinMinMax(subNode, true).NodeKey);
+        }
+
+        [TestMethod]
+        public void NodeMinSearch()
+        {
+            tree.AddKeyValue(12, 12);
+            for (int i = 0; i < 10; ++i)
+            {
+                tree.AddKeyValue(i, i);
+            }
+            BSTNode<int> subNode = tree.FindNodeByKey(2).Node;
+            Assert.AreEqual(2, tree.FinMinMax(subNode, false).NodeKey);
+        }
+
+        [TestMethod]
+        public void NodeRemoval()
+        {
+            tree.AddKeyValue(10, 10);
+            tree.AddKeyValue(9, 9);
+            tree.AddKeyValue(8, 8);
+            tree.AddKeyValue(12, 12);
+            tree.AddKeyValue(15, 15);
+            tree.AddKeyValue(11, 11);
+            tree.AddKeyValue(13, 13);
+
+            Assert.IsTrue(tree.FindNodeByKey(10).NodeHasKey);
+            tree.DeleteNodeByKey(10);
+            Assert.AreEqual(11, tree.FindNodeByKey(0).Node.RightChild.NodeKey);
         }
     }
 }
--- 14.binary search tree/Binary search tree/BST.cs
+++ 14.binary search tree/Binary search tree/BST.cs
@@ -47,31 +47,145 @@ namespace AlgorithmsDataStructures2
 
         public BSTFind<T> FindNodeByKey(int key)
         {
-            // ищем в дереве узел и сопутствующую информацию по ключу
-            return null;
+            return IterateThroughNodes(Root, key);
+        }
+        
+        private BSTFind<T> IterateThroughNodes(BSTNode<T> node, int key)
+        {
+            if (node == null)
+            {
+                return new BSTFind<T>();
+            }
+
+            int comparisonResult = key.CompareTo(node.NodeKey);
+            
+            switch(comparisonResult)
+            {
+                case -1:
+                    if (node.LeftChild == null)
+                    {
+                        BSTFind<T> nodePlaceholder = new BSTFind<T>();
+                        nodePlaceholder.Node = node;
+                        nodePlaceholder.NodeHasKey = false;
+                        nodePlaceholder.ToLeft = true;
+                        return nodePlaceholder;
+                    }
+
+                    return IterateThroughNodes(node.LeftChild, key);
+                case 0:
+                    BSTFind<T> formattedNode = new BSTFind<T>();
+                    formattedNode.Node = node;
+                    formattedNode.NodeHasKey = true;
+
+                    return formattedNode;
+                case 1:
+                    if (node.RightChild == null)
+                    {
+                        BSTFind<T> nodePlaceholder = new BSTFind<T>();
+                        nodePlaceholder.Node = node;
+                        nodePlaceholder.NodeHasKey = false;
+                        nodePlaceholder.ToLeft = false;
+                        return nodePlaceholder;
+                    }
+
+                    return IterateThroughNodes(node.RightChild, key);
+            }
+            return new BSTFind<T>();
         }
 
         public bool AddKeyValue(int key, T val)
         {
-            // добавляем ключ-значение в дерево
-            return false; // если ключ уже есть
+            BSTFind<T> searchResult = IterateThroughNodes(Root, key);
+            if (searchResult.Node == null)
+            {
+                Root = new BSTNode<T>(key, val, null);
+                return true;
+            }
+
+            if (searchResult.NodeHasKey)
+            {
+                return false; // если ключ уже есть
+            }
+
+            if (key < searchResult.Node.NodeKey)
+            {
+                searchResult.Node.LeftChild = new BSTNode<T>(key, val, searchResult.Node);
+            } else
+            {
+                searchResult.Node.RightChild = new BSTNode<T>(key, val, searchResult.Node);
+            }
+
+            return true;
         }
 
         public BSTNode<T> FinMinMax(BSTNode<T> FromNode, bool FindMax)
         {
-            // ищем максимальный/минимальный ключ в поддереве
-            return null;
+            BSTNode<T> node = FromNode;
+
+            if (!FindMax)
+            {
+                while (node.LeftChild != null)
+                {
+                    node = node.LeftChild;
+                }
+            } else
+            {
+                while (node.RightChild != null)
+                {
+                    node = node.RightChild;
+                }
+            }
+            return node;
         }
 
         public bool DeleteNodeByKey(int key)
         {
-            // удаляем узел по ключу
-            return false; // если узел не найден
+            BSTFind<T> searchResult = FindNodeByKey(key);
+
+            if (!searchResult.NodeHasKey)
+            {
+                return false; // если узел не найден
+            }
+
+            BSTNode<T> ancestorNode = searchResult.Node.LeftChild;
+            if (searchResult.Node.RightChild != null)
+            {
+                ancestorNode = searchResult.Node.RightChild;
+                while (ancestorNode.LeftChild != null)
+                {
+                    ancestorNode = ancestorNode.LeftChild;
+                }
+            }
+
+            if (searchResult.Node == searchResult.Node.Parent.LeftChild)
+            {
+                searchResult.Node.Parent.LeftChild = ancestorNode;
+            } else
+            {
+                searchResult.Node.Parent.RightChild = ancestorNode;
+            }
+            return true;
         }
 
         public int Count()
         {
-            return 0; // количество узлов в дереве
+            int counter = 1;
+            return InDepthTraversing(Root, counter);
+        }
+
+        private int InDepthTraversing(BSTNode<T> node, int counter)
+        {
+            if (node.LeftChild != null)
+            {
+                InDepthTraversing(node.LeftChild, ++counter);
+            }
+
+            if (node.RightChild != null)
+            {
+                InDepthTraversing(node.RightChild, ++counter);
+            }
+
+            return counter;
         }
 
     }
