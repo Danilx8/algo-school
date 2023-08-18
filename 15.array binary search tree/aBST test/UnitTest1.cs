8f6bc873c4ee4c9eaab32b7bd8b1929463c7a665
Луговских Данил Викторович <lugovskihdanil@yandex.ru>
2023-08-18 23:52:55 +0700

    first iteration that passed tests

--- 15.array binary search tree/aBST test/UnitTest1.cs
+++ 15.array binary search tree/aBST test/UnitTest1.cs
@@ -3,9 +3,32 @@ namespace aBST_test
     [TestClass]
     public class UnitTest1
     {
-        [TestMethod]
-        public void TestMethod1()
+        AlgorithmsDataStructures2.aBST tree;
+        int TREE_DEPTH = 2;
+
+        [TestInitialize]
+        public void TestInitialize()
         {
+            tree = new AlgorithmsDataStructures2.aBST(TREE_DEPTH);
+            tree.AddKey(50);
+            tree.AddKey(100);
+            tree.AddKey(90);
+            tree.AddKey(200);
+            tree.AddKey(25);
+            tree.AddKey(10);
+            tree.AddKey(40);
+        }
+
+        [TestMethod]
+        public void ExamineTree()
+        {
+            Assert.IsNotNull(tree.FindKeyIndex(50));
+            Assert.IsNotNull(tree.FindKeyIndex(100));
+            Assert.IsNotNull(tree.FindKeyIndex(90));
+            Assert.IsNotNull(tree.FindKeyIndex(200));
+            Assert.IsNotNull(tree.FindKeyIndex(25));
+            Assert.IsNotNull(tree.FindKeyIndex(10));
+            Assert.IsNotNull(tree.FindKeyIndex(40));
         }
     }
 }
--- 15.array binary search tree/aBST/aBST.cs
+++ 15.array binary search tree/aBST/aBST.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Security.Cryptography;
 
 namespace AlgorithmsDataStructures2
 {
@@ -11,22 +12,65 @@ namespace AlgorithmsDataStructures2
         {
             // правильно рассчитайте размер массива для дерева глубины depth:
             int tree_size = 0;
+            tree_size = CalculateTreeSize(depth, tree_size);
             Tree = new int?[tree_size];
             for (int i = 0; i < tree_size; i++) Tree[i] = null;
         }
 
+        private int CalculateTreeSize(int depth, int treeSize)
+        {
+            treeSize += Convert.ToInt32(Math.Pow(2, depth));
+            --depth;
+            if (depth < 0)
+            {
+                return treeSize;
+            }
+            return CalculateTreeSize(depth, treeSize);
+        }
+
         public int? FindKeyIndex(int key)
         {
-            // ищем в массиве индекс ключа
+            int childIndex = 1;
+            for (int i = 0; i < Tree.Length; i = i * 2 + childIndex)
+            {
+                if (Tree[i] == null)
+                {
+                    return -i;
+                }
+
+                int difference = key.CompareTo(Tree[i]);
+
+                switch (difference)
+                {
+                    case -1:
+                        childIndex = 1;
+                        break;
+                    case 0:
+                        return i;
+                    case 1:
+                        childIndex = 2;
+                        break;
+                }
+            }
             return null; // не найден
         }
 
         public int AddKey(int key)
         {
-            // добавляем ключ в массив
-            return -1;
-            // индекс добавленного/существующего ключа или -1 если не удалось
-        }
+            int? foundIndex = FindKeyIndex(key);
+            if (foundIndex == null)
+            {
+                return -1;
+            }
 
+            if (foundIndex == 0 && Tree[0] == null || foundIndex < 0)
+            {
+                foundIndex = -foundIndex;
+                Tree[(int)foundIndex] = key;
+                return (int)foundIndex;
+            }
+
+            return (int)foundIndex;
+        }
     }
 }
