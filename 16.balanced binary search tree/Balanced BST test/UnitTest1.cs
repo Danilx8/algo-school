42504342240125408f2e220af16530e855f3fd84
Луговских Данил Викторович <lugovskihdanil@yandex.ru>
2023-08-19 14:24:40 +0700

    first balanced bst iteration

--- 16.balanced binary search tree/Balanced BST test/UnitTest1.cs
+++ 16.balanced binary search tree/Balanced BST test/UnitTest1.cs
@@ -1,11 +1,37 @@
+using AlgorithmsDataStructures2;
+
 namespace Balanced_BST_test
 {
     [TestClass]
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void OddLengthTest()
         {
+            int[] input = { 90, 25, 40, 200, 100, 50, 10 };
+
+            int[] expectedOutput = { 50, 25, 100, 10, 40, 90, 200 };
+
+            CollectionAssert.AreEqual(expectedOutput, BalancedBST.GenerateBBSTArray(input));
+        }
+
+        [TestMethod]
+        public void EvenLengthTest()
+        {
+            int[] input = { 90, 25, 40, 200, 50, 100, 10, 250};
+
+            int[] expectedOutput = new int[15];
+
+            expectedOutput[0] = 50;
+            expectedOutput[1] = 25;
+            expectedOutput[2] = 100;
+            expectedOutput[3] = 10;
+            expectedOutput[4] = 40;
+            expectedOutput[5] = 90;
+            expectedOutput[6] = 250;
+            expectedOutput[13] = 200;
+
+            CollectionAssert.AreEqual(expectedOutput, BalancedBST.GenerateBBSTArray(input));
         }
     }
 }
--- 16.balanced binary search tree/Balanced BST/Balanced BST.cs
+++ 16.balanced binary search tree/Balanced BST/Balanced BST.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace AlgorithmsDataStructures2
 {
@@ -7,7 +8,48 @@ namespace AlgorithmsDataStructures2
     {
         public static int[] GenerateBBSTArray(int[] a)
         {
-            return null;
+            int treeSize = (int)Math.Pow(2, Math.Floor(Math.Log(a.Length, 2)) + 1) - 1;
+            int[] genereatedTree = new int[treeSize];
+            
+            if (a.Length == 0)
+            {
+                return genereatedTree;
+            }
+
+            Array.Sort(a);
+            AddNewElement(genereatedTree, a, 0);
+            
+            return genereatedTree;
+        }
+
+        private static void AddNewElement(int[] tree, int[] subArray, int destinationIndex)
+        {
+            int pivot = subArray.Length / 2;
+            if (pivot % 2 == 0 && pivot != 0)
+            {
+                --pivot;
+            }
+            tree[destinationIndex] = subArray[pivot];
+
+            int[] leftSide = new int[pivot];
+            int[] rightSide = new int[subArray.Length - pivot - 1];
+
+            Array.Copy(subArray, 0, leftSide, 0, pivot);
+            Array.Copy(subArray, pivot + 1, rightSide, 0, subArray.Length - pivot - 1);
+
+            if (leftSide.Length == 0 || leftSide == Array.Empty<int>() && rightSide.Length == 0 || rightSide == Array.Empty<int>())
+            {
+                return;
+            }
+
+            if (leftSide.Length != 0 || leftSide == Array.Empty<int>())
+            {
+                AddNewElement(tree, leftSide, destinationIndex * 2 + 1);
+            }
+            if (rightSide.Length != 0 || leftSide == Array.Empty<int>())
+            {
+                AddNewElement(tree, rightSide, destinationIndex * 2 + 2);
+            } 
         }
     }
 }
