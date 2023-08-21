295f496c5fd313fb7e1074b17dd589302545ebba
Луговских Данил Викторович <lugovskihdanil@yandex.ru>
2023-08-21 11:53:19 +0700

    passed tests verstion

--- 17.balanced nodes binary search tree/Balanced nodes BST test/UnitTest1.cs
+++ 17.balanced nodes binary search tree/Balanced nodes BST test/UnitTest1.cs
@@ -1,11 +1,144 @@
+using AlgorithmsDataStructures2;
+
 namespace Balanced_nodes_BST_test
 {
     [TestClass]
     public class UnitTest1
     {
-        [TestMethod]
-        public void TestMethod1()
+        BalancedBST oddLengthTree = new BalancedBST();
+        BalancedBST evenLengthTree = new BalancedBST();
+
+        [TestInitialize]
+        public void TestInitialize()
         {
+            oddLengthTree.GenerateTree(new int[] { 90, 25, 40, 200, 100, 50, 10 });
+            evenLengthTree.GenerateTree(new int[] { 90, 25, 40, 200, 50, 100, 10, 250 });
+        }
+
+        [TestMethod]
+        public void CheckLevels()
+        {
+            Assert.AreEqual(0, oddLengthTree.Root.Level);
+
+            BSTNode node = oddLengthTree.Root;
+
+            int currentLevel = 1;
+            while (node.LeftChild != null)
+            {
+                node = node.LeftChild;
+                Assert.AreEqual(currentLevel, node.Level);
+                ++currentLevel;
+            }
+
+            node = oddLengthTree.Root;
+            currentLevel = 1;
+            while (node.RightChild != null)
+            {
+                node = node.RightChild;
+                Assert.AreEqual(currentLevel, node.Level);
+                ++currentLevel;
+            }
+
+            Assert.AreEqual(0, evenLengthTree.Root.Level);
+
+            node = evenLengthTree.Root;
+
+            currentLevel = 1;
+            while (node.LeftChild != null)
+            {
+                node = node.LeftChild;
+                Assert.AreEqual(currentLevel, node.Level);
+                ++currentLevel;
+            }
+
+            node = evenLengthTree.Root;
+            currentLevel = 1;
+            while (node.RightChild != null)
+            {
+                node = node.RightChild;
+                Assert.AreEqual(currentLevel, node.Level);
+                ++currentLevel;
+            }
+        }
+
+        [TestMethod]
+        public void CheckNodesOrder()
+        {
+            BSTNode node = oddLengthTree.Root;
+            Assert.IsTrue(IsInOrder(node));
+
+            node = evenLengthTree.Root;
+            Assert.IsTrue(IsInOrder(node));
+        }
+
+        private bool IsInOrder(BSTNode node)
+        {
+            if (node == null || node.RightChild == null && node.LeftChild == null)
+            {
+                return true;
+            }
+
+            if (node.LeftChild != null && node.LeftChild.NodeKey >= node.NodeKey)
+            {
+                return false;
+            }
+
+            if (node.RightChild != null && node.RightChild.NodeKey < node.NodeKey)
+            {
+                return false;
+            }
+
+            return IsInOrder(node.LeftChild) && IsInOrder(node.RightChild);
+        }
+
+        [TestMethod]
+        public void CheckTreeBalance()
+        {
+            BSTNode node = oddLengthTree.Root;
+            Assert.IsTrue(OddCheckEveryNode(node, oddLengthTree.IsBalanced(node)));
+
+            node = evenLengthTree.Root;
+            Assert.IsTrue(EvenCheckEveryNode(node, evenLengthTree.IsBalanced(node)));
+        }
+
+        private bool OddCheckEveryNode(BSTNode node, bool isBalanced)
+        {
+            if (!isBalanced)
+            {
+                return false;
+            }
+
+            if (node.LeftChild != null)
+            {
+                OddCheckEveryNode(node.LeftChild, oddLengthTree.IsBalanced(node.LeftChild));
+            }
+
+            if (node.RightChild != null)
+            {
+                OddCheckEveryNode(node.RightChild, oddLengthTree.IsBalanced(node.RightChild));
+            }
+
+            return true;
+        }
+
+        private bool EvenCheckEveryNode(BSTNode node, bool isBalanced)
+        {
+            if (!isBalanced)
+            {
+                return false;
+            }
+
+            if (node.LeftChild != null)
+            {
+                EvenCheckEveryNode(node.LeftChild, evenLengthTree.IsBalanced(node.LeftChild));
+            }
+
+            if (node.RightChild != null)
+            {
+                EvenCheckEveryNode(node.RightChild, evenLengthTree.IsBalanced(node.RightChild));
+            }
+
+            return true;
         }
     }
 }
--- 17.balanced nodes binary search tree/Balanced nodes BST/Balanced nodes BST.cs
+++ 17.balanced nodes binary search tree/Balanced nodes BST/Balanced nodes BST.cs
@@ -34,12 +34,93 @@ namespace AlgorithmsDataStructures2
         {
             // создаём дерево с нуля из неотсортированного массива a
             // ...
+            if (a.Length != 0)
+            {
+                Array.Sort(a);
+                TreeAssembly(null, a);
+            }
+        }
+
+        private BSTNode TreeAssembly(BSTNode parent, int[] array)
+        {
+            int pivot = array.Length / 2;
+            if (pivot != 0 && pivot % 2 == 0)
+            {
+                --pivot;
+            }
+
+            BSTNode node = new BSTNode(array[pivot], parent);
+            if (parent == null)
+            {
+                node.Level = 0;
+                Root = node;
+            } else
+            {
+                node.Level = parent.Level + 1;
+            }
+
+            int[] leftSide = new int[pivot];
+            int[] rightSide = new int[array.Length - pivot - 1];
+
+            Array.Copy(array, 0, leftSide, 0, pivot);
+            Array.Copy(array, pivot + 1, rightSide, 0, array.Length - pivot - 1);
+            if (leftSide.Length != 0 || leftSide == Array.Empty<int>())
+            {
+                node.LeftChild = TreeAssembly(node, leftSide);
+            }
+            if (rightSide.Length != 0 || leftSide == Array.Empty<int>())
+            {
+                node.RightChild = TreeAssembly(node, rightSide);
+            }
+
+            return node;
         }
 
         public bool IsBalanced(BSTNode root_node)
         {
-            return false; // сбалансировано ли дерево с корнем root_node
+            return AreSubtreesLengthsEqual(root_node); // сбалансировано ли дерево с корнем root_node
         }
 
+        private bool AreSubtreesLengthsEqual(BSTNode node)
+        {
+            bool result;
+
+            int leftLength = CalculateSubTreeDepth(node.LeftChild, 0, true);
+            int rightLength = CalculateSubTreeDepth(node.RightChild, 0, false);
+
+            if (Math.Abs(leftLength - rightLength) > 1)
+            {
+                return false;
+            }
+
+            if (leftLength == 0 && rightLength == 0)
+            {
+                return true;
+            }
+
+            result = AreSubtreesLengthsEqual(node.LeftChild) && AreSubtreesLengthsEqual(node.RightChild);
+
+            return result;
+        }
+
+        private int CalculateSubTreeDepth(BSTNode node, int counter, bool leftDirection)
+        {
+            if (node == null)
+            {
+                return counter;
+            }
+
+            if (leftDirection && node.LeftChild != null)
+            {
+                CalculateSubTreeDepth(node.LeftChild, ++counter, leftDirection);
+            }
+
+            if (!leftDirection && node.RightChild != null)
+            {
+                CalculateSubTreeDepth(node.RightChild, ++counter, leftDirection);
+            }
+
+            return counter;
+        }
     }
 }
