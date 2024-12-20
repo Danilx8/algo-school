84949d32766fc32de9b8daabae61067bec062004
Danilx8 <lugovskihdanil@yandex.ru>
2024-12-20 10:56:08 +0300

    [Python] Расширил сумму нескольких списков до неограниченного количества списков. получил сложность вычислений O(x)

--- 01.linked list/summ.py
+++ 01.linked list/summ.py
@@ -1,15 +1,17 @@
 from python.linked_list import LinkedList
+from functools import reduce
 
 
-def main(first: LinkedList, second: LinkedList):
-    if first.len() != second.len():
-        return
+def main(lists: [LinkedList]) -> [int]:
+    length = lists[0].len()
+    if not map(lambda x: x.len() == length, lists):
+        raise Exception('lists must have same length')
 
     result = []
-    node1 = first.head
-    node2 = second.head
-    for i in range(first.len()):
-        result += [node1.val + node2.val]
-        node1 = node1.next
-        node2 = node2.next
+
+    nodes = [linked_list.head for linked_list in lists]
+    for _ in range(len(lists)):
+        result.append(reduce(lambda x, y: x.value + y.value, nodes))
+        map(lambda x: x.next(), nodes)
+
     return result
