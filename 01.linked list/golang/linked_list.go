e36299dc2ab8be6270b370cefddd3cf4ab6688be
Danilx8 <lugovskihdanil@yandex.ru>
2025-12-06 00:23:04 +0300

    вынес сумму в отдельный файл

--- 01.linked list/golang/linked_list.go
+++ 01.linked list/golang/linked_list.go
@@ -144,24 +144,3 @@ func (l *LinkedList) Clean() {
 	l.head = nil
 	l.tail = nil
 }
-
-func Summ(first, second *LinkedList) *LinkedList {
-	if first == nil || second == nil {
-		return nil
-	}
-	if first.Count() != second.Count() {
-		return nil
-	}
-
-	result := &LinkedList{}
-	n1 := first.head
-	n2 := second.head
-	for n1 != nil {
-		v1 := n1.value
-		v2 := n2.value
-		result.AddInTail(Node{value: v1 + v2})
-		n1 = n1.next
-		n2 = n2.next
-	}
-	return result
-}
